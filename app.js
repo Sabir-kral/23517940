347db798e0ee6ab8a67fc126bcfa21240d4b396d
Sabir-kral <sabirmemmedli21152014@gmail.com>
2024-11-23 13:21:28 +0400

    dsgfasdfsDRF

---
+++ app.js
@@ -0,0 +1,11 @@
+let eded = +prompt("birinci ededi daxil et")
+let eded2 = +prompt("ikinci ededi daxil et")
+let eded3 = +prompt("ucuncu ededi daxil et")
+if (eded>=eded2 && eded>=eded3) {
+console.log(`${eded}`)
+}
+ else if(eded2>=eded&&eded2>=eded3) {
+    console.log(`${eded2}`)
+} else if(eded3>=eded&&eded3>=eded2){
+    console.log(`${eded3}`);
+}
